c1779b73f2d89831bb1867af195d00f266a4f9ce
= <=>
2022-07-10 14:55:18 -0300

    gitignore file

--- index.py
+++ index.py
@@ -1,36 +1,89 @@
 import pandas as pd
 import assets.dataFuncs as df
-import assets.utils as ut
 
 table = pd.read_excel("excel.xlsx")
 
-cols = ["call_time", "date", "isCrossed","cross_id", "crossed_time"]
+newCols = [
+  "call_time",
+  "date",
+  "isCrossed",
+  "cross_id",
+  "crossed_time"
+]
 
-df.createHeaders(table, cols)
+useCols = {
+  "START": "inicio",
+  "END": "final",
+  "NAME": "nome"
+}
 
-df.setTotalTime(table, "inicio", "final", "call_time")
+df.createHeaders(table, newCols)
 
-names = df.getValuesFromTable(table, "nome")
-personal_tables = df.getPersonalTables(table, names, "nome")
+df.setTotalTime(
+  table,
+  useCols["START"],
+  useCols["END"],
+  newCols[0]
+)
 
-df.setDay(table, personal_tables, "inicio", "date")
+names = df.getValuesFromTable(
+  table, useCols["NAME"]
+)
+
+personal_tables = df.getPersonalTables(
+  table,
+  names,
+  useCols["NAME"]
+)
+
+df.setDay(
+  table,
+  personal_tables,
+  useCols["START"],
+  newCols[1]
+)
 
 for pTable in personal_tables: 
-  diff_days = df.getDiferentDays(pTable, "inicio")
+  diff_days = df.getDiferentDays(
+    pTable,
+    useCols["START"]
+  )
 
-  daily_tables = df.getPersonalTables(pTable, diff_days, "date")
+  daily_tables = df.getPersonalTables(
+    pTable,
+    diff_days,
+    newCols[1]
+  )
 
   for dayTable in daily_tables:
-    dayTable = df.setCrossAttr(dayTable, "isCrossed", "cross_id", ["inicio", "final"])
+    dayTable = df.setCrossAttr(
+      dayTable,
+      newCols[2],
+      newCols[3],
+      [
+        useCols["START"],
+        useCols["END"]
+      ]
+    )
+
     df.replacer(pTable, dayTable)
 
   df.replacer(table, pTable)
 
-idTableNames = df.getValuesFromIds(table, "cross_id")
-idTables = df.getPersonalTables(table, idTableNames, "cross_id")
+idTableNames = df.getValuesFromIds(
+  table,
+  newCols[3]
+)
+idTables = df.getPersonalTables(
+  table,
+  idTableNames,
+  newCols[3]
+)
 
 for idTable in idTables:
-  idTable["crossed_time"] = idTable["call_time"].sum()
+  
+  idTable[ newCols[4] ] = idTable[ newCols[0] ].sum()
+
   df.replacer(table, idTable)
 
 table.to_excel("files/step1.xlsx", index=False)
